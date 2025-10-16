d00df314273a037b3bba2d4ef2f641d1ef839ff6
JorgePeralta6 <andresjorgeperaltam@outlook.es>
2025-10-16 09:54:26 -0600

    Actu

--- src/products/product.routes.js
+++ src/products/product.routes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { check } from "express-validator";
 import { validateJWT } from "../middlewares/validate-jwt.js";
 import { validateFields } from "../middlewares/validate-fields.js";
-import { deleteProduct, getProduct, saveProduct, updateProduct, getProductStats,getProductSalesPercentage,getEarnings,getTopProduct,getTop3 } from "./product.controller.js";
+import { deleteProduct, getProduct, saveProduct, updateProduct } from "./product.controller.js";
 
 const router = Router();
 
@@ -38,16 +38,5 @@ router.delete(
     deleteProduct
 )
 
-
-
-router.get('/stats', getProductStats);
-
-router.get('/percentage', getProductSalesPercentage);
-
-router.get('/earnings', getEarnings);
-
-router.get('/top', getTopProduct);
-
-router.get('/top3', getTop3);
 
 export default router
